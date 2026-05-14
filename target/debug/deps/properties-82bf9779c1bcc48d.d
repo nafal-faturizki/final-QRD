a68a4e5bea1ca6aec3544a0713c42d598290e386
vetocatprotocol-web <vetocatprotocol@gmail.com>
2026-05-14 09:33:42 +0000

    gitigone update

--- target/debug/deps/properties-82bf9779c1bcc48d.d
+++
@@ -1,5 +0,0 @@
-/workspaces/final-QRD/target/debug/deps/properties-82bf9779c1bcc48d.d: core/qrd-core/tests/properties.rs
-
-/workspaces/final-QRD/target/debug/deps/properties-82bf9779c1bcc48d: core/qrd-core/tests/properties.rs
-
-core/qrd-core/tests/properties.rs:
--- target/debug/deps/rand_chacha-af5aecfe9b7e0dad.d
+++
@@ -1,9 +0,0 @@
-/workspaces/final-QRD/target/debug/deps/rand_chacha-af5aecfe9b7e0dad.d: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/workspaces/final-QRD/target/debug/deps/librand_chacha-af5aecfe9b7e0dad.rlib: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/workspaces/final-QRD/target/debug/deps/librand_chacha-af5aecfe9b7e0dad.rmeta: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
-
-/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs:
-/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs:
-/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs:
